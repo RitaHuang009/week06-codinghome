8a7f7a66c4e1707cc96d13427dbdad309f7c0472
ritahuang <ritahuang@hitrust.com.tw>
2022-08-22 09:36:23 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,36 @@
+$(function () {
+
+  $('.js-burger').on('click', function (e) {
+    console.log(`yes`);
+    e.preventDefault();
+    $('.js-menu-mobile').toggleClass('hidden')
+  })
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+  });
+
+  var swiper = new Swiper(".shareSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".share.swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".share.swiper-button-next",
+      prevEl: ".share.swiper-button-prev",
+    },
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+  });
+
+});
