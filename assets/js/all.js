48b71920b8ad411e7954b2e3bdd9ee87cd11a254
ritahuang <ritahuang@hitrust.com.tw>
2022-08-22 09:37:24 +0800

    Update 2022-08-22T01:37:22.148Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,36 +1,35 @@
+"use strict";
+
 $(function () {
-
   $('.js-burger').on('click', function (e) {
-    console.log(`yes`);
+    console.log("yes");
     e.preventDefault();
-    $('.js-menu-mobile').toggleClass('hidden')
-  })
-
+    $('.js-menu-mobile').toggleClass('hidden');
+  });
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     spaceBetween: 16,
     centeredSlides: true,
     loop: true,
     autoplay: {
-      delay: 3000,
-    },
+      delay: 3000
+    }
   });
-
   var swiper = new Swiper(".shareSwiper", {
     slidesPerView: "auto",
     spaceBetween: 30,
     centeredSlides: true,
     pagination: {
-      el: ".share.swiper-pagination",
+      el: ".share.swiper-pagination"
     },
     navigation: {
       nextEl: ".share.swiper-button-next",
-      prevEl: ".share.swiper-button-prev",
+      prevEl: ".share.swiper-button-prev"
     },
     loop: true,
     autoplay: {
-      delay: 3000,
-    },
+      delay: 3000
+    }
   });
-
 });
+//# sourceMappingURL=all.js.map
